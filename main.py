47ae90a3e73690516bba42bc399af88336318fc2
Gsingh225 <100107232+Gsingh225@users.noreply.github.com>
2023-02-24 16:25:43 -0500

    Worked on db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,  request, redirect, url_for
 from tortoise import fields, models, Tortoise
+from tortoise.exceptions import IntegrityError
 
 class User(models.Model):
     username = fields.CharField(max_length=50, unique=True)
@@ -7,11 +8,11 @@ class User(models.Model):
     cash = fields.DecimalField(max_digits=10, decimal_places=2)
 
 async def initalizeDB():
-    await Tortoise.init(db_url='sqlite://db.users', modules={'models': ['__main__']})
+    await Tortoise.init(db_url='sqlite://usersdb.sqlite3', modules={'models': ['__main__']})
     await Tortoise.generate_schemas()
 
 async def create_user(username: str, password: str, cash: float) -> None:
-    user = await User(username=username, password=password, cash=cash)
+    user = await User(username=username, password=password, cash=float(cash))
     await user.save()
 
 async def get_user_with_username(username: str):
@@ -19,7 +20,7 @@ async def get_user_with_username(username: str):
     return user
 
 async def deduct_from_bal(username: str, deduct: float) -> None:
-    user = get_user_with_username(username=username)
+    user = await get_user_with_username(username=username)
     user.cash -= deduct
     await user.save()
 
@@ -49,8 +50,10 @@ def register():
         cash = request.form['cash']
         try:
             create_user(username=username,  password=passs, cash=cash)
-        except:
-            return render_template('register.html', err_msg='<h5 id="oops">Account Creation Failed: try entering a number less than 10 total digits, and up to only 2 decimal places like "13462.33". If you have complied with that requirment, simply pick another username,  as that one may be taken</h5>')
+        except ValueError:
+            return render_template('register.html', err_msg='<h5 id="oops">Account Creation Failed: try entering a number less than 10 total digits, and up to only 2 decimal places like "13462.33" in the funding field.</h5>')
+        except IntegrityError:
+            return render_template('register.html', err_msg='<h5 id="oops">The Username is taken.</h5>')
         return f'successfull, user is {username} and pass is {passs} and money is {cash}'
     elif request.method == 'GET':
         return render_template('register.html', err_msg='')
