8891d91463410c09931321066a1963e53054be82
Gsingh225 <100107232+Gsingh225@users.noreply.github.com>
2023-02-24 15:10:14 -0500

    updated register func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,  request
+from flask import Flask, render_template,  request, redirect, url_for
+from tortoise import fields, models, Tortoise
 
 app = Flask(__name__)
 
@@ -12,11 +13,20 @@ def index():
         if action == 'login':
             return render_template('acc.html')
         elif action == 'register':
-            return render_template('register.html')
+            return redirect(url_for('register'))
     elif request.method == 'GET':
         return render_template('index.html')
 
-#
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['userr']
+        passs = request.form['passs']
+        cash = request.form['cash']
+        return 'hi'
+    elif request.method == 'GET':
+        return render_template('register.html')
+
 if __name__ == '__main__':
     app.run()
     
